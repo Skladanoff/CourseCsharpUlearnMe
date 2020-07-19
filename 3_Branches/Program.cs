a6082430a6919b41a147e1374c06ae772e4c53dd
Skladanoff <skladanov2@yandex.ru>
2020-07-19 18:45:16 +0400

    start

---
+++ 3_Branches/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3_Branches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MiddleOf(5, 0, 100)); // => 5
+            Console.WriteLine(MiddleOf(12, 12, 11)); // => 12
+            Console.WriteLine(MiddleOf(1, 1, 1)); // => 1
+            Console.WriteLine(MiddleOf(2, 3, 2));
+            Console.WriteLine(MiddleOf(8, 8, 8));
+            Console.WriteLine(MiddleOf(5, 0, 1));
+            Console.WriteLine(MiddleOf(21, 27, 27));
+        }
+        
+        public static int MiddleOf(int a, int b, int c)
+        {
+            if ((a >= b && b >= c) | (c >= b && b >= a)) return b;
+            else if ((a >= c && c >= b) | (b >= c && c >= a)) return c;
+            else return a;
+        }
+    }
+}
